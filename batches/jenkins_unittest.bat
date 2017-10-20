33260d99c158abc5f61030e198f1d76d93f98402
julioa16 <juliapandino@gmail.com>
2017-10-20 10:14:20 +0200

    change

--- batches/jenkins_unittest.bat
+++ batches/jenkins_unittest.bat
@@ -1,1 +1,2 @@
+C:\Jenkins\workspace\Julia_starting\StartingPoint_2005_0\bin\Debug\StartingPoint.exe
 C:jenkins\tools\nunit3-console.exe Test_Starting\bin\Debug\Test_Starting.dll --result=Testresult.xml
